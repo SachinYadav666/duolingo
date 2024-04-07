97d05c1787833a61d5136601ba1061fb7d4a3821
Sachin Yadav <sachintmg666@gmail.com>
2024-04-07 22:28:09 +0530

    add sidebar

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,27 @@
+
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 export const LearnPage = () => {
   return (
-   <div>
-    LearnPage
+
+   <div className="flex flex-row-reverse gap-[48px] px-6 ">
+    <StickyWrapper>
+    <UserProgress
+    activeCourse={{ title: "spanish", imageSrc: "/es.svg" }}
+    hearts={0}
+    points={0}
+    hasActiveSubscription={false}
+/>
+
+    </StickyWrapper>
+    <FeedWrapper>
+      <Header title="spanish"/>
+    
+    </FeedWrapper>
    </div>
   )
-}
+};
 export default LearnPage;
